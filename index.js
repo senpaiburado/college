d3dae03806a39c99251dc56e9102e8602c37ce6c
vladislav.cholak <vlad.hoc10@gmail.com>
2019-01-13 01:22:44 +0200

    Added new block and new ddos protection

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const ddos_lib = require("ddos");
+const { RateLimiterMemory } = require("rate-limiter-flexible");
 const app = express();
 
-var ddos = new ddos_lib;
 var path = require("path");
 let config = require("./config.json")
 
+const ddosParams = {
+	points: 7,
+	duration: 1
+}
+
+const limiter = new RateLimiterMemory(ddosParams)
+const rateLimiterMiddleware = (req, res, next) => {
+	limiter.consume(req.connection.remoteAddress).
+		then(() => {
+			next()
+		})
+		.catch((rejRes) => {
+			res.status(429).send("Too many requests, please try later.")
+		})
+}
+
 // Protect our site from DDOS.
-//app.use(ddos.express);
+app.use(rateLimiterMiddleware);
 // Set EJS to be used.
 app.set("view engine", "ejs");
 
@@ -15,15 +30,19 @@ app.set("view engine", "ejs");
 app.use("/temporary_not_working", express.static(__dirname + "/temporary_not_working"));
 app.use("/pages", express.static(__dirname + "/pages"));
 
-if (config["is_under_maintenance"]) {
-	app.get("/*", function(request, response) {
-    	response.render(path.join(__dirname + "/temporary_not_working/index.ejs"));
-	});
-} else {
-	app.get("/", function(request, response) {
-		response.render(path.join(__dirname + "/pages/MainPage/index.ejs"))
-	});
-}
 
-app.listen(25565);
+
+app.get("/", function(request, response) { 
+	response.render(path.join(__dirname + "/pages/MainPage/index.ejs"), {
+		maintenance: config["is_under_maintenance"]
+	})
+});
+
+// function loadTopMenusFromDb() {
+// 	var list = [];
+// 	list.append({"Main", "http://college-chnu.cv.ua/", [{"Additional", "http://college-chnu.cv.ua/article?id=1"}]})
+// 	return list;
+// }
+
+app.listen(80);
 
